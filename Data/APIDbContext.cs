a04c64a767e3213ec343ab4ffc7887c34a7460f1
Henrique Mendes <henriquemendes13@gmail.com>
2024-05-09 08:22:17 -0300

    Add project files.

---
+++ Data/APIDbContext.cs
@@ -0,0 +1,15 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace APICatalogo.Data;
+
+public class APIDbContext : DbContext
+{
+    public APIDbContext(DbContextOptions<APIDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Categoria>? Categorias { get; set; }
+    public DbSet<Produto>? Produtos { get; set; }
+}
